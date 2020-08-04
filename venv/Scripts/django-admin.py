37d3e29483e55b8ad1a440a40d82003f1052a457
omar115 <omarhasan115@gmail.com>
2020-08-04 12:53:11 +0600

    final addition

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\akash\PycharmProjects\face_recognition_kinect\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
